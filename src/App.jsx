1d2e072f8965df646c1895e68745474e837c1947
unknown <sharma.803061@gmail.comProfile image>
2026-03-27 17:40:18 +0600

    added a frist file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Countries from './compoent/countries/Countries'
+
+function App() {
+
+  return (
+    <>
+        <div>
+          <h1>Get started</h1>
+          <Countries></Countries>
+        </div>
+    </>
+  )
+}
+
+export default App
