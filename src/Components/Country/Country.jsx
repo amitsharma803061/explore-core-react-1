54aa737d8cbefd5aea9c9eb301b4b98efc003abb
unknown <sharma.803061@gmail.comProfile image>
2026-03-29 20:21:51 +0600

    added last file

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
-    console.log(country.region.region)
+const Country = ({ country, handleVisitedCountries, handleVisitedFlag }) => {
+    const [visited, setVisited] = useState(false);
+    // console.log(country.area.area)
+
+
+    const handleVisited = () => {
+        // first system
+        // if(visited) {
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        // second system 
+        // setVisited(visited ? false : true);
+
+        // third system 
+        setVisited(!visited)
+        handleVisitedCountries(country)
+    }
+
+
     return (
-        <div className='Country'>
+        // <div className={`country border-lg text-center ${visited ? 'country-visited' : 'country-not-visited'}`}>
+        <div className={`Country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2>Name: {country.name.common}</h2>
             <h2>Continents: {country.continents.continents}</h2>
             <h2>Population: {country.population.population}</h2>
             <h2>Capital: {country.capital.capital}</h2>
-            <h2>Languages:</h2>
+            <h2>Area: {country.area.area},  {
+                country.area.area > 300000 ? "big country" : 'small country'
+            }</h2>
             <h2>Region: {country.region.region}</h2>
+            <div className='button-container'>
+                <button onClick={handleVisited} className='visited'>
+                    {visited ? 'Visited' : 'Not Visited'}
+                </button>
+                <button onClick={() => { handleVisitedFlag(country.flags.flags.png)}} className='addFlags'>
+                    Add visited flags
+                </button>
+            </div>
         </div>
     );
 };
